07b50325ad626fffa21a0c75511a36f60c61e6cf
sumitanjana1910-netizen <sumitanjana1910@gmail.com>
2026-06-30 06:50:05 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,7 @@
-
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <a href="/login" className="text-purple-500 text-xl font-bold underline">Go to Login</a>
+    </div>
+  );
+}
