cda29d58ff7790a5ef3b7561e91ca1991473cf2e
sumitanjana1910-netizen <sumitanjana1910@gmail.com>
2026-06-30 07:15:59 +0530

    Delete app/page.js

--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <a href="/login" className="text-purple-500 text-xl font-bold underline">Go to Login</a>
-    </div>
-  );
-}
