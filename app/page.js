334610c318af3757ac72df3d6d024017289a3efa
sumitanjana1910-netizen <sumitanjana1910@gmail.com>
2026-06-30 07:33:21 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Rex Admin Panel</h1>
+      <a href="/login">Login</a> | <a href="/dashboard">Dashboard</a>
+    </div>
+  );
+}
