6766e9d34bd99377837b3d4e63f213cf99e0c11f
sumitanjana1910-netizen <sumitanjana1910@gmail.com>
2026-06-30 07:33:58 +0530

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Admin Login</h1>
+      <p>Login Page is working!</p>
+    </div>
+  );
+}
