232233e4349574db007a26747e6faad4ee8bf15e
sumitanjana1910-netizen <sumitanjana1910@gmail.com>
2026-06-30 23:03:38 +0530

    Create page.js

---
+++ ​app/resellers/page.js
@@ -0,0 +1,9 @@
+export default function Resellers() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Resellers List</h1>
+      <p>Yahan tumhare saare resellers ki list dikhegi.</p>
+      {/* Yahan baad mein hum database se data fetch karenge */}
+    </div>
+  );
+}
